369457d14226830adb002605c31f3521a5584f8c
Ilias <i.elhammouti@student.fontys.nl>
2023-12-07 10:17:11 +0100

    Initial commit

---
+++ src/main/java/com/mappy/tagservice/TagserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mappy.tagservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagserviceApplication.class, args);
+	}
+
+}
