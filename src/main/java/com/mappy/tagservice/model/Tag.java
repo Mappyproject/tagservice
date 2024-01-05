a1075e65f3ae2f639b396167120c531bc2a5073e
Iliaselh <ilias@primeitsolutions.nl>
2024-01-06 00:22:25 +0100

    added generated id

--- src/main/java/com/mappy/tagservice/model/Tag.java
+++ src/main/java/com/mappy/tagservice/model/Tag.java
@@ -1,6 +1,8 @@
 package com.mappy.tagservice.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,6 +16,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Tag {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
